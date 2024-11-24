d7caf29d6fd95cab3e0b5b680502a1d47d9ef8c6
jakobistjakob <j.j.martin@web.de>
2024-11-24 15:12:51 +0100

    Text sollte jetzt untereinander stehen.

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -138,11 +138,11 @@
 						</div>
 					</div>
 
-					<div class="text-3xl sm:text-4xl line-clamp-1" in:fade={{ duration: 100 }} style="font-size: 2rem;">
-						Benutzt die o1-Modelle bitte sparsam.
+					<div class="text-3xl sm:text-4xl line-clamp-1" in:fade={{ duration: 100 }} style="font-size: 2rem; display: block">
+						o1-Modelle bitte sparsam benutzen
 					</div>
-					<div class="text-3xl sm:text-4xl line-clamp-1" in:fade={{ duration: 100 }} style="font-size: 1.5rem;">
-						Für sensible Daten benutzt bitte ein open-source Model.
+					<div class="text-3xl sm:text-4xl line-clamp-1" in:fade={{ duration: 100 }} style="font-size: 1.5rem; display : block">
+						Bei sensiblen Daten: open-source Modelle.
 					</div>
 				</div>
 
